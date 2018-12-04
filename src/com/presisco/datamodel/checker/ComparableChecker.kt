d6e6cb8c8a654c79a9a3d3a990fc9530e34b8594
presisco <internight@sina.com>
2018-12-04 10:56:37 +0800

    update to 1.1.2 add null check to ComparableChecker

--- src/com/presisco/datamodel/checker/ComparableChecker.kt
+++ src/com/presisco/datamodel/checker/ComparableChecker.kt
@@ -1,15 +1,27 @@
 package com.presisco.datamodel.checker
 
+
 open class ComparableChecker<T>(
         private val min: Comparable<T>,
-        private val max: Comparable<T>
-) : Checker<Comparable<T>>() {
-    override fun check(item: Comparable<T>) = (item >= min as T && item <= max as T)
-    override fun trim(item: Comparable<T>): Comparable<T> {
-        return if (item >= min as T && item <= max as T) {
-            item
-        } else {
+        private val max: Comparable<T>,
+        private val nullable: Boolean = false,
+        private val default: Comparable<T> = max
+) : Checker<Comparable<T>?>() {
+    override fun check(item: Comparable<T>?): Boolean {
+        if (item == null)
+            return nullable
+        return (item >= min as T && item <= max as T)
+    }
+
+    override fun trim(item: Comparable<T>?): Comparable<T> {
+        return if (item == null) {
+            default
+        } else if (item > max as T) {
             max
+        } else if (item < min as T) {
+            min
+        } else {
+            item
         }
     }
 }
